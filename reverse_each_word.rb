251d81c2c6e9d5c508a19c990aeccee038761740
Luis Martinez <luis_mmartinez@live.com>
2020-12-28 03:40:07 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,6 +3,6 @@ def reverse_each_word(sentence)
   string_to_array= sentence.split
   filling_array= string_to_array.collect {|element| element.reverse}
   filling_string= filling_array.join
-  bniding.pry
+  binding.pry
 
 end
