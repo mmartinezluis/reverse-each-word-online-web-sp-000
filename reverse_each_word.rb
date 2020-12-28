892b4c137b29c929f826d16e7059fe59ba67d81f
Luis Martinez <luis_mmartinez@live.com>
2020-12-28 03:41:30 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,7 @@
+require "pry"
+def reverse_each_word(sentence)
+  string_to_array= sentence.split
+  filling_array= string_to_array.collect {|element| element.reverse}
+  filling_string= filling_array.join(" ")
+  return filling_string
+end
